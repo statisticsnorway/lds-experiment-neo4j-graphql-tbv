160542eb96595406558f1e05d56508fac987437d
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-06-11 12:26:01 +0200

    Update neo and plugin versions

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-for i in neo4j:3.5; do
-  docker pull $i &
+for i in neo4j:4.0; do
+  docker pull $i
 done
 
 mkdir -p neo4jplugins
-
-if [ ! -f "apoc-3.5.0.4-all.jar" ]; then
+pushd neo4jplugins
+if [ ! -f "apoc-4.0.0.14-all.jar" ]; then
+  rm -f apoc-*
   echo "Downloading neo4j apoc plugin...";
-  pushd neo4jplugins
-  curl -L -O https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.5.0.4/apoc-3.5.0.4-all.jar
-  popd
+  curl -L -O https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.14/apoc-4.0.0.14-all.jar
   echo "Download complete!";
 fi
+popd
