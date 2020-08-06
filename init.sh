c02dfa9eacb42df169f976aa5d76a4620fd38614
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-08-06 09:43:08 +0200

    Use Neo4j 4.1

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-for i in neo4j:4.0; do
+for i in neo4j:4.1; do
   docker pull $i
 done
 
 mkdir -p neo4jplugins
 pushd neo4jplugins
-if [ ! -f "apoc-4.0.0.14-all.jar" ]; then
+if [ ! -f "apoc-4.1.0.1-all.jar" ]; then
   rm -f apoc-*
   echo "Downloading neo4j apoc plugin...";
-  curl -L -O https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.14/apoc-4.0.0.14-all.jar
+  curl -L -O https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.1/apoc-4.1.0.1-all.jar
   echo "Download complete!";
 fi
 popd
