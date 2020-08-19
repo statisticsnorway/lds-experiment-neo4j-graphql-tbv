3758749367df007e28ada5eebf839e79273dc387
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-08-19 22:42:25 +0200

    Switch default example

--- src/main/java/no/ssb/neo4j/graphql/tbv/Main.java
+++ src/main/java/no/ssb/neo4j/graphql/tbv/Main.java
@@ -1,10 +1,10 @@
 package no.ssb.neo4j.graphql.tbv;
 
-import no.ssb.neo4j.graphql.tbv.examples.PersonMovieExample;
+import no.ssb.neo4j.graphql.tbv.examples.MixMapAndArrayExample;
 
 public class Main {
 
     public static void main(String[] args) {
-        new ExampleRunner(new PersonMovieExample()).run();
+        new ExampleRunner(new MixMapAndArrayExample()).run();
     }
 }
