0f8e9b02ef74eb578f31b0a0348d818712362c39
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-09-07 07:26:11 +0200

    Update apoc version

--- init.sh
+++ init.sh
@@ -6,10 +6,10 @@ done
 
 mkdir -p neo4jplugins
 pushd neo4jplugins
-if [ ! -f "apoc-4.1.0.1-all.jar" ]; then
+if [ ! -f "apoc-4.1.0.2-all.jar" ]; then
   rm -f apoc-*
   echo "Downloading neo4j apoc plugin...";
-  curl -L -O https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.1/apoc-4.1.0.1-all.jar
+  curl -L -O https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.1.0.2/apoc-4.1.0.2-all.jar
   echo "Download complete!";
 fi
 popd
